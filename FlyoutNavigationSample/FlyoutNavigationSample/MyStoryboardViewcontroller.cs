4eef197aebade942ab968dd634be6208dc3582fd
James Clancey <james.clancey@gmail.com>
2014-12-11 16:16:56 -0900

    Added new samples

---
+++ FlyoutNavigationSample/FlyoutNavigationSample/MyStoryboardViewcontroller.cs
@@ -0,0 +1,14 @@
+using System;
+using Foundation;
+using UIKit;
+using System.CodeDom.Compiler;
+
+namespace Sample
+{
+	partial class MyStoryboardViewController : UIViewController
+	{
+		public MyStoryboardViewController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
